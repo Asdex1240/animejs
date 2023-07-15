1fcdb46041e3972ea4dbb840738de4a700a2d5c2
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2023-07-15 15:59:03 -0500

    Subimos proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements AfterViewInit{
+  divs = [
+    { value: 200, corredor: 1},
+    { value: 100, corredor: 2},
+    { value: 50, corredor: 3},
+    { value: 150, corredor: 4},
+    { value: 123, corredor: 5},
+  ]
   constructor() {}
+  animation!: any
+  ngAfterViewInit(): void {
+    const value = [200,100,50,150,123]
+    const elements = document.querySelectorAll('.miElemento')
+    this.animation = anime({
+      targets: elements,
+      width: '100%',
+      duration: 1000,
+      loop: false,
+      delay: function(el, i){
+        return value[i]
+      },
+      autoplay: false,
+      direction: 'alternate',
+      easing: 'easeInOutQuad'
+    })
+  }
 
 }
